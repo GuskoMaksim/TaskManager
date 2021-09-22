6eb2177df78e12907132ef3e5457ccb92f251403
Husko Maksim <gusko.maksim.n@gmail.com>
2021-09-22 23:24:48 +0300

    Added project files

---
+++ TaskManager/TaskManager.cpp
@@ -0,0 +1,12 @@
+﻿// TaskManager.cpp: определяет точку входа для приложения.
+//
+
+#include "TaskManager.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
